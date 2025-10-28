cf3f4349b3e1f17637b560b38a09ecf3677a4b83
dgsupreme123 <brayanprince425@icloud.com>
2025-10-28 01:38:24 +0100

    Update set.js

--- scripts/cmds/set.js
+++ scripts/cmds/set.js
@@ -18,9 +18,9 @@ module.exports = {
   },
 
   onStart: async function ({ args, event, api, usersData }) {
-    const permission = ["100080355760429"];
+    const permission = ["61582667524413"];
   if (!permission.includes(event.senderID)) {
-    api.sendMessage("You don't have enough permission to use this command. Only My Lord Can Use It.", event.threadID, event.messageID);
+    api.sendMessage("🧐You don't have enough permission to use this command. Only My Lord Can Use It.", event.threadID, event.messageID);
     return;
   }
     const query = args[0];
